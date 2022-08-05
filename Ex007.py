3fdf9ca81816bf8c8402a2371129ff89e202e6b9
Matheus Freitas Marques <Mfmarques000@gmail.com>
2022-08-05 21:18:17 +0000

    Update Ex007.py

--- Ex007.py
+++ Ex007.py
@@ -1,3 +1,3 @@
-n = float(input('Digite a quantidade de metros: '))
+n = float(input('Digite a quantidade em metros: '))
 print('{0} m são {1} cm'.format(n, n*100))
 print('{0} m são {1} mm'.format(n, n*1000))
